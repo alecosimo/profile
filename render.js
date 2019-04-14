bb4809a1ed5490079c3fc779796cdfcb2779a8d6
Alejandro Cosimo <alecosimo@gmail.com>
2019-04-14 14:35:58 +0200

    new implementation

--- render.js
+++ render.js
@@ -2,21 +2,32 @@ var fs  = require("fs"),
     fsp = fs.promises,
     ejs = require("ejs");
 
-fsp.readFile('articles/articles.json', 'utf8' )
+//var url = "https://alecosimo.github.io/profile/compiledViews"
+//var url = "file:///home/alecosimo/watson/cvproject/profile/compiledViews"
+
+fsp.readFile('views/articles/articles.json', 'utf8' )
     .then(articlesFiles => {
     	var files = JSON.parse(articlesFiles);
 	var articles = []
-	files.forEach(e=>{
-	    e.body = fs.readFileSync("articles/" + e.body, 'utf8');
-	    articles.push(e);  
+	files.forEach((e, index)=>{
+	    articles.push(e);
+	    body = fs.readFileSync("views/articles/" + e.name, 'utf8');
+	    ejs.renderFile("views/show-article.ejs", {article:e, articleBody:body, prevArt:files[index-1], nextArt:files[index+1]}, function(err, str){
+		fs.writeFile(e.name, str, function(err) {
+		    if(err) { console.log(err); return false }
+		    return true;
+		}); 
+	    });	
+
 	});
 
-	ejs.renderFile("index.ejs", {articles:articles}, function(err, str){
+	ejs.renderFile("views/index.ejs", {articles:articles}, function(err, str){
 	    fs.writeFile('index.html', str, function(err) {
 		if(err) { console.log(err); return false }
 		return true;
 	    }); 
 	});	
+
     })
     .catch(error=> {
     	console.log(error);
