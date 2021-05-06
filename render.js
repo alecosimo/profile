65a1ee158956d5cd8dba7c588c22de1a47265335
Alejandro Cosimo <alecosimo@gmail.com>
2021-05-06 11:03:57 +0200

    updated render.js to lastest version of ejs

--- render.js
+++ render.js
@@ -1,36 +1,39 @@
-var fs  = require("fs"),
-    fsp = fs.promises,
-    ejs = require("ejs");
+var fs = require('fs'),
+	fsp = fs.promises,
+	ejs = require('ejs');
 
 //var url = "https://alecosimo.github.io/profile/compiledViews"
-//var url = "file:///home/alecosimo/watson/cvproject/profile/compiledViews"
+//var url = "file:///home/alecosimo/watson/profile/compiledViews"
 
-fsp.readFile('views/articles/articles.json', 'utf8' )
-    .then(articlesFiles => {
-    	var files = JSON.parse(articlesFiles);
-	var articles = []
-	files.forEach((e, index)=>{
-	    articles.push(e);
-	    body = fs.readFileSync("views/articles/" + e.name, 'utf8');
-	    ejs.renderFile("views/show-article.ejs", {article:e, articleBody:body, prevArt:files[index-1], nextArt:files[index+1]}, function(err, str){
-		fs.writeFile(e.name, str, function(err) {
-		    if(err) { console.log(err); return false }
-		    return true;
-		}); 
-	    });	
+fsp.readFile('views/articles/articles.json', 'utf8')
+	.then(articlesFiles => {
+		var files = JSON.parse(articlesFiles);
+		var articles = []
 
+		files.forEach((e, index) => {
+			articles.push(e);
+			body = fs.readFileSync("views/articles/" + e.name, 'utf8');
+			ejs.renderFile("views/show-article.ejs", { article: e, articleBody: body, prevArt: files[index - 1], nextArt: files[index + 1] },
+				{}, (err, str) => {
+					if (err) { console.log(err); throw err; }
+					fs.writeFile(e.name, str, (err) => {
+						if (err) { console.log(err); return false }
+						return true;
+					});
+				});
+		});
+
+		ejs.renderFile("views/index.ejs", { articles: articles }, function (err, str) {
+			fs.writeFile('index.html', str, function (err) {
+				if (err) { console.log(err); return false }
+				return true;
+			});
+		});
+
+	})
+	.catch(error => {
+		console.log(error);
 	});
 
-	ejs.renderFile("views/index.ejs", {articles:articles}, function(err, str){
-	    fs.writeFile('index.html', str, function(err) {
-		if(err) { console.log(err); return false }
-		return true;
-	    }); 
-	});	
-
-    })
-    .catch(error=> {
-    	console.log(error);
-    });
 
 
